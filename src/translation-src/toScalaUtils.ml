b46982b31e552918f0da5fe169018ccfa3cee030
Louis Jachiet <louis.jachiet@inria.fr>
2016-07-19 16:02:47 +0200

    Simplification of functions in toScalaUtils.

--- src/translation-src/toScalaUtils.ml
+++ src/translation-src/toScalaUtils.ml
@@ -31,7 +31,7 @@ let upEnv (x:element) (env:environment) =
     | t :: q -> if String.compare x.var t.var = 0
       then aux(x,q,x :: result) (* We add the new version of 'x'. *)
       else begin 
-	if List.mem t.var x.varlist = true
+	if List.mem t.var x.varlist
 	then let newt = {var=t.var;label=x.label;nbJoin=t.nbJoin;varlist=x.varlist} in
 	     aux(x,q,newt :: result) (* We change the version of 't'. *)
 	else aux(x,q,t :: result) (* We just keep scanning. *)
@@ -45,13 +45,10 @@ let upEnv (x:element) (env:environment) =
 
 (* The idea of 'adjust' is to provide a modified 'l1' where duplicates with 'l2' are a bit changed
    e.g. l1=[a;b;c] l2=[a;x;y] adjust(l1,l2)=[a1;b;c] *)
+   
 let adjust (l1:string list) (l2:string list):string list =
-  let rec aux l1 l2 lresult = match l1 with
-  | [] -> List.rev lresult
-  | t :: q -> if List.mem t l2 = true
-    then aux q l2 ((t^"bis") :: lresult) (* Obviously, a problem can occured if a variable has been named ?xbis also... *)
-    else aux q l2 (t :: lresult)
-  in aux l1 l2 []
+ (* Obviously, a problem can occured if a variable has been named ?xbis also... *)
+ List.map (fun t -> if List.mem t l2 then t^"bis" else t) l1 
 ;;
 
 let printlist(l:string list):string = 
@@ -92,29 +89,14 @@ let return_biggest_label(env:environment):int=
 
 
 let contained l1 l2 = (* We assuming that there are no duplicate values in l1 and l2 *)
-  let rec aux l1 l2 n = match l1 with
-    | [] -> n
-    | t :: q -> if List.mem t l2 = true
-      then aux q l2 (n+1)
-      else aux q l2 n
-  in if List.length l1 = aux l1 l2 0
-    then true
-    else false
+  List.for_all (fun x -> List.mem x l2) l1
 ;;
 
 (* Return the element that having a varlist containing dist and having the biggest label *)
 let return_good((dist:string list) , (env:environment) ):element =
-  let rec aux d e el = match e with
-    | [] -> el
-    | t::q -> if contained d t.varlist = true
-      then begin
-	if el.label <= t.label
-	then aux d q t
-	else aux d q el
-      end else
-	aux d q el
-  in let initial:element={var="";label=0;nbJoin=0;varlist=[]} in
-     aux dist env.suite initial
+  let initial:element={var="";label=0;nbJoin=0;varlist=[]} in
+  List.fold_left (fun ac el -> if el.label > ac.label && contained dist el.varlist then el else ac)  initial env.suite
+
 ;;
 
 (* The function to translate the distingushed variables. *)
