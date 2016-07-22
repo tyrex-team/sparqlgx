7f227b4ad0fd34d75b60bb38c8f7c0eaf962661f
Louis Jachiet <louis.jachiet@inria.fr>
2016-07-22 11:59:02 +0200

    PATHes

--- tests/run-statistics.sh
+++ tests/run-statistics.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+PATH_CMD=$(dirname $(dirname $0))
 echo "== Generating Statistic Files (Lubm and Watdiv) =="
-bash ../bin/generate-stat.sh sparqlgx-test/lubm/init.triples stat-lubm.txt
-bash ../bin/generate-stat.sh sparqlgx-test/watdiv/init.triples stat-watdiv.txt
+bash ${PATH_CMD}/bin/generate-stat.sh sparqlgx-test/lubm/init.triples stat-lubm.txt
+bash ${PATH_CMD}/bin/generate-stat.sh sparqlgx-test/watdiv/init.triples stat-watdiv.txt
 echo ""
 
 exit 0
