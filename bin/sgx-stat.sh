a7324eafbd2db3766cca9b5d1645f3427b148316
dgraux <damien.graux@inria.fr>
2016-08-02 14:13:53 +0200

    Proper Statistic Generation Script

---
+++ bin/sgx-stat.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PATH_CMD=$(dirname $0) ;
+
+source ${PATH_CMD}/../conf/sparqlgx.conf
+
+if [[ $# != 2 ]];
+then
+    echo "Usage: $0 dbName tripleFile_HDFSPath"
+    exit 1
+fi
+
+dbName=$1
+tripleFile=$2
+
+localdbpath=$(sed "s|~|$HOME|g" <<< "$SPARQLGX_LOCAL/$dbName")
+
+spark-submit --driver-memory $SPARK_DRIVER_MEM \
+    --executor-memory $SPARK_EXECUT_MEM \
+    --class=GenerateStat \
+    ${PATH_CMD}/sparqlgx-stat-0.1.jar $tripleFile $SPARQLGX_STAT_SIZE \
+    > $localdbpath/stat.txt ;
+
+exit 0
