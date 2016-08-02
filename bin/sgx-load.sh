c5ac79805c86ef8bbd2786e7e0ce0b4845796b49
dgraux <damien.graux@inria.fr>
2016-08-02 14:19:47 +0200

    A Load Script

---
+++ bin/sgx-load.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PATH_CMD=$(dirname $0) ;
+
+source ${PATH_CMD}/../conf/sparqlgx.conf
+
+if [[ $# != 2 ]];
+then
+    echo "Usage: $0 dbName tripleFile_HDFSPath"
+    exit 1
+fi
+
+dbName=$1
+tripleFile=$2
+
+localdbpath=$(sed "s|~|$HOME|g" <<< "$SPARQLGX_LOCAL/$dbName")
+hdfsdbpath="$SPARQLGX_HDFS/$dbName/"
+
+mkdir -p $localdbpath ;
+hadoop fs -mkdir -p $hdfsdbpath ;
+
+spark-submit --driver-memory $SPARK_DRIVER_MEM \
+    --executor-memory $SPARK_EXECUT_MEM \
+    --class=Load \
+    ${PATH_CMD}/sparqlgx-load-0.1.jar $tripleFile $hdfsdbpath ;
+
+exit 0
