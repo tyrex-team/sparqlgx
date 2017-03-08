1a2d7ff9390c5b24fd5c66629a0db033917cefdb
dgraux <damien.graux@inria.fr>
2017-03-08 11:43:19 +0100

    SPARQL HDFS Path in tests

--- tests/clean-all.sh
+++ tests/clean-all.sh
@@ -5,7 +5,7 @@ PATH_CMD=$(dirname $0)/../bin/
 nbStep="2"
 # Cleaning HDFS resources and results.
 echo -e "[1/$nbStep]  HDFS test directory\tremoved"
-hadoop fs -rm -r sparqlgx-test &> /dev/null
+hadoop fs -rm -r $SPARQLGX_HDFS/sparqlgx-test &> /dev/null
 # Purging databases.
 echo -e "[2/$nbStep]  Databases\t\tremoved"
 bash ${PATH_CMD}/sparqlgx.sh remove lubm &> /dev/null
--- tests/preliminaries.sh
+++ tests/preliminaries.sh
@@ -3,15 +3,17 @@
 logs=" /dev/null"
 PATH_CMD=$(dirname $0)
 
+source $PATH_CMD/../conf/sparqlgx.conf
+
 nbStep="3"
 # HDFS Directories.
-echo -e "[1/$nbStep]  HDFS test directories\tcreated at\tsparqlgx-test/"
-hadoop fs -mkdir sparqlgx-test/ &> $logs
-hadoop fs -mkdir sparqlgx-test/results/ &> $logs
+echo -e "[1/$nbStep]  HDFS test directories\tcreated at\t$SPARQLGX_HDFS/sparqlgx-test/"
+hadoop fs -mkdir $SPARQLGX_HDFS/sparqlgx-test/ &> $logs
+hadoop fs -mkdir $SPARQLGX_HDFS/sparqlgx-test/results/ &> $logs
 # Copy N-Triples RDF files.
-echo -e "[2/$nbStep]  Dataset 1 (lubm.nt)\tadded in\tsparqlgx-test/"
-hadoop fs -copyFromLocal ${PATH_CMD}/resources/datasets/lubm.nt sparqlgx-test/ &> $logs
-echo -e "[3/$nbStep]  Dataset 2 (watdiv.nt)\tadded in\tsparqlgx-test/"
-hadoop fs -copyFromLocal ${PATH_CMD}/resources/datasets/watdiv.nt sparqlgx-test/ &> $logs
+echo -e "[2/$nbStep]  Dataset 1 (lubm.nt)\tadded in\t$SPARQLGX_HDFS/sparqlgx-test/"
+hadoop fs -copyFromLocal ${PATH_CMD}/resources/datasets/lubm.nt $SPARQLGX_HDFS/sparqlgx-test/ &> $logs
+echo -e "[3/$nbStep]  Dataset 2 (watdiv.nt)\tadded in\t$SPARQLGX_HDFS/sparqlgx-test/"
+hadoop fs -copyFromLocal ${PATH_CMD}/resources/datasets/watdiv.nt $SPARQLGX_HDFS/sparqlgx-test/ &> $logs
 
 exit 0
