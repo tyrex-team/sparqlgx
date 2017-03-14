24f0ce555fcf240709af076240b74e60ca0a44d5
Louis Jachiet <louis.jachiet@inria.fr>
2017-03-14 14:56:11 +0100

    Pretty print on bench + keeping logs + refactoring run-benchmarks

--- tests/run-benchmarks.sh
+++ tests/run-benchmarks.sh
@@ -14,7 +14,6 @@
 ##                                                                   ##
 #######################################################################
 
-logs="logs"
 PATH_SGX=$(dirname $0)/../bin/
 source ${PATH_SGX}/../conf/sparqlgx.conf
 
@@ -34,54 +33,77 @@ echo -e "\tStatistic size:\t\t$SPARQLGX_STAT_SIZE"
 echo -e "\tExecution with:\t\tspark-submit"
 echo -e "\t\tDriver   ->\t$SPARK_DRIVER_MEM"
 echo -e "\t\tExecutor ->\t$SPARK_EXECUT_MEM"
-echo ""
 # Query names.
-LQ="Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9 Q10 Q11 Q12 Q13 Q14"
-WQ="C1 C2 C3 F1 F2 F3 F4 F5 L1 L2 L3 L4 L5 S1 S2 S3 S4 S5 S6 S7"
+QUERIES[0]="Q1 Q2 Q3 Q4 Q5 Q6 Q7 Q8 Q9 Q10 Q11 Q12 Q13 Q14";
+QUERIES[1]="C1 C2 C3 F1 F2 F3 F4 F5 L1 L2 L3 L4 L5 S1 S2 S3 S4 S5 S6 S7";
+DATASET[0]=$SPARQLGX_HDFS/sparqlgx-test/lubm.nt ;
+DATASET[1]=$SPARQLGX_HDFS/sparqlgx-test/watdiv.nt ;
+BENCHNAME[0]="lubm";
+BENCHNAME[1]="watdiv";
 # Time beginning.
 start_time=$(date +%s)
 # Loads.
-echo "--------------------- LOAD ----------------------"
-t1_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh light-load lubm $SPARQLGX_HDFS/sparqlgx-test/lubm.nt &>> $logs ; t1_end=$(date +%s);
-t2_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh generate-stat lubm $SPARQLGX_HDFS/sparqlgx-test/lubm.nt &>> $logs ; t2_end=$(date +%s);
-echo "> LUBM dataset loaded in $(($t1_end-$t1_beg))s and its statistics generated in $((t2_end-$t2_beg))s."
-t1_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh light-load watdiv $SPARQLGX_HDFS/sparqlgx-test/watdiv.nt &>> $logs ; t1_end=$(date +%s);
-t2_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh generate-stat watdiv $SPARQLGX_HDFS/sparqlgx-test/watdiv.nt &>> $logs ; t2_end=$(date +%s);
-echo "> WATDIV dataset loaded in $(($t1_end-$t1_beg))s and its statistics generated in $((t2_end-$t2_beg))s."
+tokname=$(cat ../.git/refs/heads/master)__$(date "+%Y-%m-%d_%H-%M-%S") ;
+token="$SPARQLGX_HDFS/$tokname"
+logs="logs.$tokname"                                                                                    
+ln -sf $logs logs.latest
+echo -e "\t\tToken: $tokname"
+echo -e "\t\tResults: $token"
+echo -e "\t\tLogs: $logs"
+echo "" ;
+hadoop fs -mkdir -p $token/results ;
+for b in 0 1 ;
+do
+    echo "";
+    echo "";
+    echo "";
+    echo "---------------------"
+    echo "         ${BENCHNAME[$b]}       "
+    echo "---------------------"
+    echo "";
+    echo "--------------------- LOAD ----------------------"
+    t1=$(date +%s);
+    bash ${PATH_SGX}/sparqlgx.sh light-load ${BENCHNAME[$b]} ${DATASET[$b]} &>> $logs ;
+    t2=$(date +%s);
+    bash ${PATH_SGX}/sparqlgx.sh generate-stat ${BENCHNAME[$b]} ${DATASET[$b]} &>> $logs ;
+    t3=$(date +%s);
+    echo "> ${BENCHNAME[$b]} dataset loaded in $((t2-t1))s and its statistics generated in $((t3-t2))s."
+    echo "----------------------------------------------- EVAL ----------------------------------------------------"
+    echo -e "| \t|\t     Direct Evaluation\t\t|\t\t    Standard Evaluation\t\t\t|"
+    echo -e "| Query\t|\tNo Optim\tStandard\t|\tNo Optim\tStandard\tWith Statistics\t|"
+    for i in ${QUERIES[$b]}; do
+        # The same query is done 5 times: depending on optimizations
+        # sketches (with or without any, statistics or not...).
+        tmpf=$(mktemp -u) ;
+        mkfifo $tmpf ;
+        (tail -f $tmpf)&
+        tail_pid="$!" ;
+        (
+            echo -n -e "| $i\t|";
+            t1=$(date +%s);
+            bash ${PATH_SGX}/sparqlgx.sh direct-query --no-optim -o $token/results/$i.sde.nooptim.txt $(dirname $0)/resources/queries/$i.rq ${DATASET[$b]} ;
+            t2=$(date +%s);
+            echo -n -e "\t$((t2-t1))" >> $tmpf;
+            bash ${PATH_SGX}/sparqlgx.sh direct-query -o $token/results/$i.sde.txt $(dirname $0)/resources/queries/$i.rq ${DATASET[$b]}  ;
+            t3=$(date +%s);
+            echo -n -e "\t\t$((t3-t2))">> $tmpf;
+            bash ${PATH_SGX}/sparqlgx.sh query --no-optim -o $token/results/$i.sgx.nooptim.txt ${BENCHNAME[$b]} $(dirname $0)/resources/queries/$i.rq ;
+            t4=$(date +%s);
+            echo -n -e "\t\t|\t$((t4-t3))" >> $tmpf;
+            bash ${PATH_SGX}/sparqlgx.sh query -o $token/results/$i.sgx.txt ${BENCHNAME[$b]} $(dirname $0)/resources/queries/$i.rq  ;
+            t5=$(date +%s);
+            echo -n -e "\t\t$((t5-t4))" >> $tmpf;
+            bash ${PATH_SGX}/sparqlgx.sh query --stat -o $token/results/$i.sgx.stat.txt ${BENCHNAME[$b]} $(dirname $0)/resources/queries/$i.rq  ;
+            t6=$(date +%s);
+            echo -e "\t\t$((t6-t5))\t\t|" >> $tmpf;
+        ) | sed -u "s/^/[$i] /" &>>$logs ;
+        kill $tail_pid ;
+
+    done
+    echo "---------------------------------------------------------------------------------------------------------"
+done ;
 echo ""
-# LUBM.
-echo "----------------------------------------------- EVAL LUBM -----------------------------------------------"
-echo -e "| \t|\t     Direct Evaluation\t\t|\t\t    Standard Evaluation\t\t\t|"
-echo -e "| Query\t|\tNo Optim\tStandard\t|\tNo Optim\tStandard\tWith Statistics\t|"
-for i in $LQ; do
-    # The same query is done 5 times: depending on optimizations
-    # sketches (with or without any, statistics or not...).
-    t1_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh direct-query --no-optim -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sde.nooptim.txt $(dirname $0)/resources/queries/$i.rq $SPARQLGX_HDFS/sparqlgx-test/lubm.nt &>> $logs ; t1_end=$(date +%s);
-    t2_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh direct-query -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sde.txt $(dirname $0)/resources/queries/$i.rq $SPARQLGX_HDFS/sparqlgx-test/lubm.nt &>> $logs ; t2_end=$(date +%s);
-    t3_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh query --no-optim -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sgx.nooptim.txt lubm $(dirname $0)/resources/queries/$i.rq &>> $logs ; t3_end=$(date +%s);
-    t4_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh query -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sgx.txt lubm $(dirname $0)/resources/queries/$i.rq &>> $logs ; t4_end=$(date +%s);
-    t5_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh query --stat -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sgx.stat.txt lubm $(dirname $0)/resources/queries/$i.rq &>> $logs ; t5_end=$(date +%s);
-    echo -e "| $i\t|\t$((t1_end-t1_beg))\t\t$((t2_end-t2_beg))\t\t|\t$((t3_end-t3_beg))\t\t$((t4_end-t4_beg))\t\t$((t5_end-t5_beg))\t\t|"
-done
-echo "---------------------------------------------------------------------------------------------------------"
-echo ""
-# WatDiv.
-echo "---------------------------------------------- EVAL WATDIV ----------------------------------------------"
-echo -e "| \t|\t     Direct Evaluation\t\t|\t\t    Standard Evaluation\t\t\t|"
-echo -e "| Query\t|\tNo Optim\tStandard\t|\tNo Optim\tStandard\tWith Statistics\t|"
-for i in $WQ; do
-    # The same query is done 5 times: depending on optimizations
-    # sketches (with or without any, statistics or not...).
-    t1_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh direct-query --no-optim -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sde.nooptim.txt $(dirname $0)/resources/queries/$i.rq $SPARQLGX_HDFS/sparqlgx-test/watdiv.nt &>> $logs ; t1_end=$(date +%s);
-    t2_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh direct-query -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sde.txt $(dirname $0)/resources/queries/$i.rq $SPARQLGX_HDFS/sparqlgx-test/watdiv.nt &>> $logs ; t2_end=$(date +%s);
-    t3_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh query --no-optim -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sgx.nooptim.txt watdiv $(dirname $0)/resources/queries/$i.rq &>> $logs ; t3_end=$(date +%s);
-    t4_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh query -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sgx.txt watdiv $(dirname $0)/resources/queries/$i.rq &>> $logs ; t4_end=$(date +%s);
-    t5_beg=$(date +%s); bash ${PATH_SGX}/sparqlgx.sh query --stat -o $SPARQLGX_HDFS/sparqlgx-test/results/$i.sgx.stat.txt watdiv $(dirname $0)/resources/queries/$i.rq &>> $logs ; t5_end=$(date +%s);
-    echo -e "| $i\t|\t$((t1_end-t1_beg))\t\t$((t2_end-t2_beg))\t\t|\t$((t3_end-t3_beg))\t\t$((t4_end-t4_beg))\t\t$((t5_end-t5_beg))\t\t|"
-done
-echo "---------------------------------------------------------------------------------------------------------"
-echo ""
-# End.
+
 stop_time=$(date +%s)
 
 exit 0
