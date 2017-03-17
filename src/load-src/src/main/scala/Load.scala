f581d4a66f086d8ef97c92efa622087e0b15f0b5
Louis Jachiet <louis.jachiet@inria.fr>
2017-03-17 15:14:15 +0100

    Tentative of new loading method

--- src/load-src/src/main/scala/Load.scala
+++ src/load-src/src/main/scala/Load.scala
@@ -6,6 +6,17 @@ import org.apache.spark.rdd.RDD
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import scala.util.matching.Regex
+import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
+import scala.Tuple2;
+
+class RDDMultipleTextOutputFormat<A, B> extends MultipleTextOutputFormat<A, B> {
+
+    @Override
+    protected String generateFileNameForKeyValue(A key, B value, String name) {
+        return key.toString();
+    }
+}
+
 
 object Load {
   def main(args: Array[String]) {
@@ -15,22 +26,13 @@ object Load {
     val reg = new Regex("\\s+.\\s*$") ;
     val conf = new SparkConf().setAppName("Simple Application");
     val sc = new SparkContext(conf);
-    val T = sc.textFile(args(0)).map{line => val field:Array[String]=line.split("\\s+",3); if(field.length!=3){throw new RuntimeException("Invalid line: "+line);}else{(field(0),field(1),reg.replaceFirstIn(field(2),""))}}.cache;
-    //val T = sc.textFile(args(0)).map{line => val field:Array[String]=line.split(" "); (field(0),field(1),field(2))}.cache;
+    val T = sc.textFile(args(0)).mapToPair{line => val field:Array[String]=line.split("\\s+",3); if(field.length!=3){throw new RuntimeException("Invalid line: "+line);}else{("p"+field(1).toLowerCase.map{ case c =>
+          if( (c<'a' || c>'z') && (c<'0' || c>'9')){ '_'} else {c}
+          },(field(0),reg.replaceFirstIn(field(2),"")))}}.cache;
 
     val confhadoop = sc.hadoopConfiguration
     val fshadoop = org.apache.hadoop.fs.FileSystem.get(confhadoop)
-
-    val Apred = T.map{case (s,p,o)=>p}.distinct.collect;
-    val Avalue = Apred.map{case lambda => lambda.toLowerCase.map{ case c =>
-          if( (c<'a' || c>'z') && (c<'0' || c>'9')){ '_'} else {c}
-          }};
-    
-    for( i <- 0 to Apred.length -1){
-      val exists = fshadoop.exists(new org.apache.hadoop.fs.Path(args(1)+Avalue(i)+".pred"))
-      if(!exists){
-        T.filter{case (s,p,o) => p.equals(Apred(i))}.map{case (s,p,o) => s + " " + o}.saveAsTextFile(args(1)+"p"+Avalue(i));
-      }
+    T.saveAsHadoopFile("output/", String.class, String.class,
+                        RDDMultipleTextOutputFormat.class);
     }
-  }
 }
