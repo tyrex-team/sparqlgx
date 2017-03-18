1dbe3a411cf027a2af0f0be7c87b8b795ff66d86
Louis Jachiet <louis.jachiet@inria.fr>
2017-03-18 02:21:47 +0100

    Loading in one pass

--- src/load-src/src/main/scala/Load.scala
+++ src/load-src/src/main/scala/Load.scala
@@ -8,15 +8,16 @@ import org.apache.log4j.Level
 import scala.util.matching.Regex
 import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
 import scala.Tuple2;
+import org.apache.hadoop.io.NullWritable
 
-class RDDMultipleTextOutputFormat[A, B] extends MultipleTextOutputFormat[A, B] {
 
-    override
-    protected def generateFileNameForKeyValue(key:A, value:B, name:String):String = {
-        return key.toString()+"/"+name
-    }
+class RDDMultipleTextOutputFormat extends MultipleTextOutputFormat[Any, Any] {
+    override def generateActualKey(key: Any, value: Any): Any = 
+          NullWritable.get()
+
+      override def generateFileNameForKeyValue(key: Any, value: Any, name: String): String = 
+            key.asInstanceOf[String]+"/"+name
 }
-
 
 object Load {
   def main(args: Array[String]) {
@@ -28,10 +29,10 @@ object Load {
     val sc = new SparkContext(conf);
     val T = sc.textFile(args(0)).map{line => val field:Array[String]=line.split("\\s+",3); if(field.length!=3){throw new RuntimeException("Invalid line: "+line);}else{("p"+field(1).toLowerCase.map{ case c =>
           if( (c<'a' || c>'z') && (c<'0' || c>'9')){ '_'} else {c}
-          },(field(0),reg.replaceFirstIn(field(2),"")))}}.cache;
+          },(field(0)+" "+reg.replaceFirstIn(field(2),"")))}}.cache;
 
     val confhadoop = sc.hadoopConfiguration
     val fshadoop = org.apache.hadoop.fs.FileSystem.get(confhadoop)
-    T.saveAsHadoopFile(args(1),classOf[String],classOf[String],classOf[RDDMultipleTextOutputFormat[String,String]])
+    T.saveAsHadoopFile(args(1),classOf[String],classOf[String],classOf[RDDMultipleTextOutputFormat])
     }
 }
