f5309327623b2dd6816af397037c6a33f753fbe6
Louis Jachiet <louis.jachiet@inria.fr>
2017-03-20 14:58:20 +0100

    Merging load and stat

--- src/load-src/src/main/scala/Load.scala
+++ src/load-src/src/main/scala/Load.scala
@@ -1,14 +1,16 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
+import org.apache.hadoop.io.NullWritable
+import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
+import org.apache.log4j.Level
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger
+import org.apache.log4j.Logger;
 import org.apache.spark._
 import org.apache.spark.rdd.RDD
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
-import scala.util.matching.Regex
-import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import scala.Tuple2;
-import org.apache.hadoop.io.NullWritable
+import scala.util.matching.Regex
 
 
 class RDDMultipleTextOutputFormat extends MultipleTextOutputFormat[Any, Any] {
@@ -19,7 +21,27 @@ class RDDMultipleTextOutputFormat extends MultipleTextOutputFormat[Any, Any] {
             key.asInstanceOf[String]+"/"+name
 }
 
-object Load {
+
+object Main {
+
+  def merge(xs: List[(Int,String)], ys: List[(Int,String)],n:Int): List[(Int,String)] = {
+    if (n==0) { Nil }
+    else
+      (xs, ys) match {
+        case(Nil, ys) => ys
+        case(xs, Nil) => xs
+        case(x :: xs1, y :: ys1) =>
+          if (x._1 > y._1) x::merge(xs1, ys,n-1)
+          else y :: merge(xs, ys1,n-1)
+      }
+  }
+
+
+  def combine(acc:List[(Int,String)],size:Int,el:(Int,String),numberMax:Int) : (List[(Int,String)],Int) = {
+     val na = merge(el::Nil,acc,numberMax) ;
+       (na,(size+1) min numberMax)
+  }
+
   def main(args: Array[String]) {
     // Cut of spark logs.
     Logger.getLogger("org").setLevel(Level.OFF);
@@ -27,12 +49,82 @@ object Load {
     val reg = new Regex("\\s+.\\s*$") ;
     val conf = new SparkConf().setAppName("Simple Application");
     val sc = new SparkContext(conf);
-    val T = sc.textFile(args(0)).map{line => val field:Array[String]=line.split("\\s+",3); if(field.length!=3){throw new RuntimeException("Invalid line: "+line);}else{("p"+field(1).toLowerCase.map{ case c =>
-          if( (c<'a' || c>'z') && (c<'0' || c>'9')){ '_'} else {c}
-          },(field(0)+" "+reg.replaceFirstIn(field(2),"")))}}.cache;
 
-    val confhadoop = sc.hadoopConfiguration
-    val fshadoop = org.apache.hadoop.fs.FileSystem.get(confhadoop)
-    T.saveAsHadoopFile(args(1),classOf[String],classOf[String],classOf[RDDMultipleTextOutputFormat])
+    var statsize = 500 ;
+    var hdfs_path : String = "" ;
+    var noLoad = false ;
+    var noStat = false ;
+    var tripleFile = "" ;
+    var curArg = 0 ;
+    while(curArg < args.length) {
+      args(curArg) match {
+        case "--noLoad" =>
+          noLoad = true
+        case "--hdfs-path" =>
+          if(curArg+1 == args.length)
+            throw new Exception("No hdfs-path given!");
+          hdfs_path=args(curArg+1)
+          curArg+=1;
+        case "--stat-size" =>
+          if(curArg+1 == args.length)
+            throw new Exception("No stat size given!");
+          statsize=args(curArg+1).toInt ;
+          curArg+=1 ;
+        case "--no-stat" =>
+          noStat=true;
+        case s =>
+          if(tripleFile != "")
+            throw new Exception("Invalid command line (two triple files given)!");
+          tripleFile = args(curArg)
+        }
+        curArg+=1 ;
     }
+    
+    val numberMax = statsize ;
+    val input = sc.textFile(tripleFile) ;
+
+    if(!noLoad) {
+      val T = input.map {
+        line => 
+          val field:Array[String]=line.split("\\s+",3); 
+        if(field.length!=3) { 
+          throw new RuntimeException("Invalid line: "+line);
+        }
+        else { 
+          ("p"+field(1).toLowerCase.map{ 
+            case c =>
+              if( (c<'a' || c>'z') && (c<'0' || c>'9'))
+                 '_'
+              else 
+                c
+          },(field(0)+" "+reg.replaceFirstIn(field(2),"")))}};
+
+      T.saveAsHadoopFile(args(1),classOf[String],classOf[String],classOf[RDDMultipleTextOutputFormat])
+    }
+
+    if(!noStat) {
+      val stat = input.flatMap{
+        line =>
+        val field:Array[String]=line.split("\\s+",3);
+        if(field.length!=3) {
+          throw new RuntimeException("Invalid line: "+line);
+        }
+        else {
+          List(((0,field(0)),1),((1,field(1)),1),((2,reg.replaceFirstIn(field(2),"")),1))
+        }
+      }.reduceByKey(_+_).map { t => (t._1._1,(t._2,t._1._2)) // Compute word count
+                             }.aggregateByKey( (Nil:List[(Int,String)],0) ) ( // Compute the numberMax most present per key (and key is s or p or o)
+        { case ((acc,size),el) => combine(acc,size,el,numberMax) },
+        { case ((a1,s1),(a2,s2)) => (merge(a1,a2,numberMax),((s1+s2) min numberMax)) }
+      ).collect.sortWith{case (a,b) => a._1<b._1 }
+
+      println(stat(0)._2._2.toString+" "+stat(1)._2._2.toString+" "+stat(2)._2._2.toString);
+      for(i <- 0 to 2) {
+        val list_el=stat(i)._2._1;
+        val last = list_el.last._2;
+        list_el foreach { case (n,iri) => val v = if(iri==last) "*" else iri ;  println (v+" "+n.toString) } ;
+        println();
+      }
+    }
+  }
 }
