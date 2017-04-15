b5b53b0a75ac93c819b0c431710bfd50317dd8d0
Louis Jachiet <louis.jachiet@inria.fr>
2017-04-15 13:53:58 +0200

    Bug fix -> not the same prefix in load and eval

--- src/load-src/src/main/scala/Load.scala
+++ src/load-src/src/main/scala/Load.scala
@@ -102,7 +102,7 @@ object Main {
     for( id <- 0 to a.length-1 ) {
       if(s.startsWith(a(id))) {
         //insert a : as in normal prefixes ?
-        return id.toString+s.substring(a(id).length());
+        return BigInt(id).toString(36)+s.substring(a(id).length());
       }
     }
     return s;
