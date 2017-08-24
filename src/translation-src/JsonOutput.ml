ae492c2eddd5f3bce1e9e3f528ad9a01561566ad
Louis Jachiet <louis.jachiet@inria.fr>
2017-08-24 14:46:09 +0200

    Test of bug fix in json output for broadcast joins

--- src/translation-src/JsonOutput.ml
+++ src/translation-src/JsonOutput.ml
@@ -7,7 +7,7 @@ let print a =
   let add x = list_instr := x::!list_instr in
   let cur_id = ref (-1) in
   let get_id () = incr cur_id ; !cur_id in
-  let rec foo cols x =
+  let rec foo id_of_bc cols x =
     let id = get_id() in
     let jid =  "id",`Int id in
     let assoc v l =
@@ -30,44 +30,43 @@ let print a =
      | Readfile2(f) ->
         op "PRED"  ["filename",`String ("p"^numero f);"col_subject",`String (assoc  "s" cols); "col_object",`String (assoc  "o" cols) ]
      | Filter(Equal(c,v),a) ->
-        op "FILTER"  [var_at c; var_at v ; "id",`Int (foo cols a) ]
+        op "FILTER"  [var_at c; var_at v ; "id",`Int (foo id_of_bc cols a) ]
      | Keep(l,a) ->
-        op "SELECT"  ["cols",`List (List.map (fun c -> `String (assoc c cols)) l) ; "id",`Int (foo cols a) ]
+        op "SELECT"  ["cols",`List (List.map (fun c -> `String (assoc c cols)) l) ; "id",`Int (foo id_of_bc cols a) ]
      | Join(a,b) ->
-        op "JOIN" ["id1",`Int (foo cols a) ;  "id2",`Int (foo cols b)]
+        op "JOIN" ["id1",`Int (foo id_of_bc cols a) ;  "id2",`Int (foo id_of_bc cols b)]
      | LeftJoin(a,b) ->
-        op "LEFTJOIN" ["id_base",`Int (foo cols a) ;  "id_extend",`Int (foo cols b)]
+        op "LEFTJOIN" ["id_base",`Int (foo id_of_bc cols a) ;  "id_extend",`Int (foo id_of_bc cols b)]
      | JoinWithBroadcast(b,a) ->
-        let varname = `String ("join_"^(string_of_int (get_id ()))) in
-        let _ = op "BROADCAST" ["vardef",`Int (foo cols a) ;  "varname",varname] in
-        op "MAP_BC" ["id",`Int (foo cols b) ;  "var",varname]
+        let id_bc = get_id () in
+        let _ = add (`Assoc ["op",`String "BROADCAST"; "id",`Int id_bc;"source",`Int (foo id_of_bc cols a)]) in
+        op "MAP_BC" ["id1",`Int (foo id_of_bc cols b) ;  "id2", `Int id_bc]
      | Broadcast(i,a,b) ->
-        let varname = `String ( "i_"^string_of_int i) in
-        let def = foo cols a in
-        let _ = op "BROADCAST" ["vardef",`Int def ;  "varname",varname] in
-        foo cols b
+        let id_bc = get_id () in
+        let def = foo id_of_bc cols a in
+        let _ = add (`Assoc ["op",`String "BROADCAST"; "id",`Int id_bc; "source",`Int def ]) in
+        foo ((i,id_bc)::id_of_bc) cols b
 
      | FilterWithBroadcast(a,i,_) ->
-        let varname = `String ("i_"^string_of_int i) in
-        op "MAP_BC" ["id",`Int (foo cols a) ;  "var",varname]
+        op "MAP_BC" ["id1",`Int (foo id_of_bc cols a) ;  "id2",`Int (List.assoc i id_of_bc )]
 
      | Empty ->
         op "EMPTY" []
 
      | Order (l,a) ->
-        op "ORDER" ["id",`Int (foo cols a) ; "BY",`Assoc (List.map (fun (x,y) -> (assoc x cols),`Bool y) l) ]
+        op "ORDER" ["id",`Int (foo id_of_bc cols a) ; "BY",`Assoc (List.map (fun (x,y) -> (assoc x cols),`Bool y) l) ]
       
         
      | Rename(a,b,c) ->
-        foo ((a,b)::cols) c       
+        foo id_of_bc ((a,b)::cols) c       
 
      | Union(a,b) ->
-        op "UNION"  ["id1",`Int (foo cols a) ;  "id2",`Int (foo cols b)]
+        op "UNION"  ["id1",`Int (foo id_of_bc cols a) ;  "id2",`Int (foo id_of_bc cols b)]
 
      | Distinct(a) ->
-        op "DISTINCT"  ["id",`Int (foo cols a)]
+        op "DISTINCT"  ["id",`Int (foo id_of_bc cols a)]
        
      | _ -> failwith ("error @"^__LOC__)
   in
-  let final = foo [] a in
+  let final = foo [] [] a in
   print_string (Yojson.Basic.to_string (`Assoc ["commands",`List (List.rev !list_instr); "result_id",`Int final]))
