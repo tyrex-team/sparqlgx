c24551256278a45acde3b1ae1793ad971806f408
Louis Jachiet <louis.jachiet@inria.fr>
2017-10-29 14:01:14 +0100

    bug fix

--- src/translation-src/Prefix.ml
+++ src/translation-src/Prefix.ml
@@ -1,6 +1,6 @@
 
 type trie =
-  | Node of (char*trie) list*string list
+  | Node of (char*trie) list*(string option)
   
 let add_trie t s v = 
 
@@ -8,21 +8,21 @@ let add_trie t s v =
     let rec foo = function
       | (c,t)::l when (c=s.[i]) -> (c,add_trie t (i+1))::l
       | (c,t)::l -> (c,t)::foo l
-      | [] -> [s.[i],add_trie t (i+1)]
+      | [] -> [s.[i],add_trie (Node([],None)) (i+1)]
     in
 
     match t with
     | Node(pos,pref) ->
        if i = String.length s
-       then Node(pos,v::pref)
+       then (match pref with Some w -> failwith (s^" / "^v^" / "^w^" / "^(string_of_int i)) | _ -> Node(pos,Some v))
        else Node(foo pos,pref)
   in
   add_trie t 0
 
 let find_trie t s =
   let rec foo i = function
-    | Node(a,[]) -> foo (i+1) (List.assoc s.[i] a)
-    | Node(a,x::t) ->
+    | Node(a,None) -> foo (i+1) (List.assoc s.[i] a)
+    | Node(a,Some x) ->
        try
          if String.length s > i
          then
@@ -32,7 +32,7 @@ let find_trie t s =
        with _ -> x,i
   in
   let (tr,i) = foo 0 t in
-  print_string ("// "^tr^" for "^s^" \n" );
+  (* print_string ("// "^tr^" for "^s^" \n" ); *)
   tr,i
 
 (* let t = (Node([],[]))  *)
@@ -40,7 +40,7 @@ let find_trie t s =
 (* let t =add_trie (Node([],[])) "abcd" "42"  *)
 (* let c = find_trie t "abcde"  *)
   
-let prefixes = ref (Node([],[]))
+let prefixes = ref (Node([],None))
 
 let load filename =
   let chan = Scanf.Scanning.from_file filename in
@@ -50,7 +50,7 @@ let load filename =
     with
     | End_of_file -> (Scanf.Scanning.close_in chan ; f )
   in
-  prefixes:=foo (Node([],[]))
+  prefixes:=foo (Node([],None))
   
 
 let prefixize s =
@@ -64,3 +64,7 @@ let prefixize s =
     | _ -> s
   else
     s
+
+(* let test = load "root/watdiv/prefix.txt" *)
+         
+(* let t = prefixize "<http://db.uwaterloo.ca/~galuc/wsdbm/User52825>" *)
